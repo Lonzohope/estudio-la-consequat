74b27dd8772cc61f9afd6c824607b71238a01751
lonzohope <hopemutheu@gmail.com>
2019-05-02 18:22:06 +0300

    commit changes

--- app/main/views.py
+++ app/main/views.py
@@ -46,3 +46,47 @@ def steve():
     # now_showing_news = get_news()
     title = 'Home - Welcome to The best Photo Hub Website Online'
     return render_template('steve.html', title=title)
+
+
+@main.route('/zhang')
+def zhang():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # sources=get_sources()
+    # Getting popular news
+    # popular_photos= get_photo("top-photos")
+    # upcoming_news = get_news()
+    # now_showing_news = get_news()
+    title = 'Home - Welcome to The best Photo Hub Website Online'
+    return render_template('zhang.html', title=title)
+
+@main.route('/manny')
+def manny():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # sources=get_sources()
+    # Getting popular news
+    # popular_photos= get_photo("top-photos")
+    # upcoming_news = get_news()
+    # now_showing_news = get_news()
+    title = 'Home - Welcome to The best Photo Hub Website Online'
+    return render_template('manny.html', title=title)
+
+@main.route('/lisa')
+def lisa():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # sources=get_sources()
+    # Getting popular news
+    # popular_photos= get_photo("top-photos")
+    # upcoming_news = get_news()
+    # now_showing_news = get_news()
+    title = 'Home - Welcome to The best Photo Hub Website Online'
+    return render_template('lisa.html', title=title)
+
