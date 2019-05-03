79b394a0b937173394f75f151392ab07143034cd
Wambita Fana <wambitafana@gmail.com>
2019-05-03 09:09:29 +0300

    Authentication

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mr/Desktop/estudio/virtual"
+set -gx VIRTUAL_ENV "/home/moringaschool/estudio-la-consequat/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
